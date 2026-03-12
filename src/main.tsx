7616200efa32fb780312f7bdc227081248bb268a
Natasha V200 <faithnatasha200@gmail.com>
2026-03-12 07:33:48 -0400

    change  commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import "@fontsource/inter/latin-300.css";
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-500.css";
+import "@fontsource/inter/latin-600.css";
+import "@fontsource/inter/latin-700.css";
+import "@fontsource/inter/latin-800.css";
+import "@fontsource/inter/latin-900.css";
 import App from "./App.tsx";
 import "./index.css";
 
